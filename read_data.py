8c1f71d0a599028cefcc454b5ae0a656bf1ae72c
I-am-Gabi <gabicavalcantesilva@gmail.com>
2016-08-29 11:18:05 -0300

    clean code

--- read_data.py
+++ read_data.py
@@ -1,14 +1,14 @@
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 
-def initialize(file):
-    return pd.read_csv(file, low_memory=False)
+def initialize(file_name):
+    return pd.read_csv(file_name, low_memory=False)
 
 
 def access_data(zika_data):
     # number of rows
     print 'data size: ' + str(len(zika_data))
-    #  get the column names
+    # get the column names
     print 'columns: ' + str(zika_data.columns.tolist())
     # access columns like a dictionary with string keys
     print zika_data['location']
@@ -24,19 +24,20 @@ def access_data(zika_data):
     print zika_data.location.head()
 
 
-def filtering(self):
-    self.zika_data['country'] = self.zika_data['location']
-    self.zika_data['country'] = self.zika_data['country'].astype(str)
-    self.zika_data['country'] = self.zika_data['country'].apply(lambda x: pd.Series(x.split('-')))
+def filtering(zika_data):
+    # remove state/city from country
+    zika_data['country'] = zika_data['location']
+    zika_data['country'] = zika_data['country'].astype(str)
+    zika_data['country'] = zika_data['country'].apply(lambda x: pd.Series(x.split('-')))
     # filter
-    mask_brazil = self.zika_data['country'] == "Brazil"
+    mask_brazil = zika_data['country'] == "Brazil"
     # This is a pandas Series object, which is the one-dimensional equivalent of a DataFrame.
-    print type(mask_brazil)
+    # print type(mask_brazil)
     # the filter is nothing more than a Series with a boolean value for every item in the index.
-    print mask_brazil
+    # print mask_brazil
     # pandas lines up the rows of the DataFrame and the filter using the index
     # and then keeps the rows with a True filter value.
-    brazil_frame = self.zika_data[mask_brazil]
+    brazil_frame = zika_data[mask_brazil]
 
     # Removing columns I don't need
     # Brazil reports locations at the state level
@@ -47,7 +48,20 @@ def filtering(self):
     foo = lambda x: pd.Series([i for i in reversed(x.split('-'))])
     brazil_frame['location'] = brazil_frame.location.apply(foo)
     brazil_frame.rename(columns={'location': 'state_city'}, inplace=True)
+    # print brazil_frame
+
+    # Cleaning the state city column
+    brazil_frame.state_city = brazil_frame.state_city.map(lambda x: x.replace('_', ' '))
+    brazil_frame = brazil_frame[brazil_frame.state_city != 'Brazil']
+    # print brazil_frame.state_city.value_counts()
+
+    # Keeping microcephaly_confirmed and microcephaly_fatal_confirmed
+    mask_conf = (brazil_frame['data_field'] == "microcephaly_confirmed") | (
+    brazil_frame['data_field'] == "microcephaly_fatal_confirmed")
+    brazil_frame = brazil_frame[mask_conf]
+    print (brazil_frame.data_field.value_counts())
+    print ("++++++++++++++")
 
 
-read = initialize('data/cdc_zika.csv')
-read.filtering()
+data = initialize('data/simple_zika.csv')
+filtering(data)
